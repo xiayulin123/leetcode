a5eea0cd8d5a9737e9bc8d5928f03836343791f3
xiayulin123 <xiayulinsky@gmail.com>
2023-09-18 23:15:50 -0400

    make some commits

--- happy_number.cpp
+++ happy_number.cpp
@@ -2,22 +2,16 @@ class Solution {
 public:
     bool isHappy(int n) {
         int x = 0;
-        
         while(n > 0){
             int k = pow(n%10,2);
             x+=k;
             n = n/10;
         }
-        cout<<x<<endl;
         if(x == 1 || x==7){
             return true;
         }if(x< 10){
             return false;
         }
-       
-        return isHappy(x);
-
-        
-        
+        return isHappy(x);        
     }
 };
