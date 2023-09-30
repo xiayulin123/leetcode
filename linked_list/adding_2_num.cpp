fe07d23200029227d3b60d27b5d06cf039cfb24a
xiayulin123 <xiayulinsky@gmail.com>
2023-09-30 12:38:11 -0400

    make some commits

--- linked_list/adding_2_num.cpp
+++ linked_list/adding_2_num.cpp
@@ -13,7 +13,6 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode *result = new ListNode;
         int carry = 0;
-        int newCarry = 0;
         int sum = 0;
         ListNode *current = result;
         while(l1 != nullptr || l2 != nullptr){
@@ -26,18 +25,12 @@ public:
                 sum += l2->val;
                 l2 = l2->next;
             }
-            if((sum + carry) >= 10){
-                newCarry = 1;
-                sum = (sum+carry) % 10;
-            }else{
-                newCarry = 0;
-                sum = (sum+carry);
-            }
-            ListNode *tmp = new ListNode(sum);
+            sum = sum+carry;
+            carry = sum/10;
+            
+            ListNode *tmp = new ListNode(sum%10);
             current->next = tmp;
             current = tmp;
-            carry = newCarry;
-            newCarry = 0;
         }
         if(carry == 1){
             ListNode *tmp = new ListNode(1);
