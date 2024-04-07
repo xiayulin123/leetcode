87b5508662341069ac00d760961e218438da021a
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2024-04-07 09:23:58 -0400

    Create 415_AddString.go

---
+++ 415_AddString.go
@@ -0,0 +1,17 @@
+func addStrings(num1 string, num2 string) string {
+    add := 0
+    ans := ""
+    for i, j := len(num1) - 1, len(num2) - 1; i >= 0 || j >= 0 || add != 0; i, j = i - 1, j - 1 {
+        x, y := 0, 0
+        if i >= 0 {
+            x = int(num1[i] - '0')
+        }
+        if j >= 0 {
+            y = int(num2[j] - '0')
+        }
+        result := add + x + y
+        ans = strconv.Itoa(result % 10) + ans
+        add = result / 10
+    }
+    return ans
+}
