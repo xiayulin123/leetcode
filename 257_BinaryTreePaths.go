104138d45ffe446851f6d1b4d9019337c96385ef
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2024-04-21 11:31:26 -0400

    Update 257_BinaryTreePaths.go

--- 257_BinaryTreePaths.go
+++ 257_BinaryTreePaths.go
@@ -60,3 +60,39 @@ func binaryTreePaths(root *TreeNode) []string {
     return ans
     
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func binaryTreePaths(root *TreeNode) []string {
+    var ans []string
+    if root == nil {
+        return ans
+    }
+    sub := ""
+    var dig func(root *TreeNode, sub string)
+    dig = func(root *TreeNode, sub string){
+        if root != nil {
+            tmp := sub + strconv.Itoa(root.Val)
+            if root.Left == nil && root.Right == nil {
+                ans = append(ans, tmp)
+            }else {
+                tmp += "->"
+                dig(root.Left, tmp)
+                dig(root.Right, tmp)
+
+            }
+        }
+
+    }
+    dig(root, sub)
+    return ans
+    
+}
+
