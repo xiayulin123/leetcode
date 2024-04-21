d5295f901d9831c7c683dc522ed27e70906e27bb
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2024-04-21 11:44:17 -0400

    Create 263_UglyNumber.go

---
+++ 263_UglyNumber.go
@@ -0,0 +1,19 @@
+func isUgly(n int) bool {
+    if n == 0 {
+        return false
+    }
+    if n == 1 || n == 2 || n == 5 || n == 3 {
+        return true
+    }
+    for n % 2 == 0 {
+        n /= 2
+    } 
+    for n % 3 == 0 {
+        n /= 3
+    }
+    for n % 5 == 0 {
+        n /= 5
+    }
+
+    return n == 1
+}
