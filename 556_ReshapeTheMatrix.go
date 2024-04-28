81027101322c2318d9e6badaca4febcbd82dc65f
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2024-04-27 23:33:07 -0400

    Create 556_ReshapeTheMatrix.go

---
+++ 556_ReshapeTheMatrix.go
@@ -0,0 +1,15 @@
+func matrixReshape(mat [][]int, r int, c int) [][]int {
+    n, m := len(mat), len(mat[0])
+    if n * m != r * c {
+        return mat
+    }
+    ans := make([][]int, r)
+    for i := range ans {
+        ans[i] = make([]int, c)
+    }
+
+    for i := 0; i < r * c; i++ {
+        ans[i/c][i%c] = mat[i/m][i%m]
+    }
+    return ans
+}
