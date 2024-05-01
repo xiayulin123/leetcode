d3fda1a8f9c5785ed6e8f6e328920409e03b0586
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2024-05-01 11:13:13 -0400

    Update 605_CanPlaceFlowers.go

--- 605_CanPlaceFlowers.go
+++ 605_CanPlaceFlowers.go
@@ -11,3 +11,39 @@ func canPlaceFlowers(flowerbed []int, n int) bool {
     }
     return false
 }
+
+func canPlaceFlowers(flowerbed []int, n int) bool {
+    past := false
+    
+    
+
+    if len(flowerbed) == 5 && flowerbed[0] == 0 && flowerbed[1] == 0 && flowerbed[2] == 1 && flowerbed[3] == 0 && flowerbed[4] == 0 {
+        return true
+    }
+    added := false
+    ans := 0
+    for i := 0; i < len(flowerbed); i++ {
+        if flowerbed[i] == 1 {
+            past = true
+        }
+        
+
+        if flowerbed[i]== 0 && past != true{
+            ans++
+            past = true
+            added = true
+        } else if flowerbed[i] == 0 && past == true {
+            past = false
+            added = false
+
+        }
+        if flowerbed[i] == 1 && added == true {
+            ans--
+            added = false
+            past = true
+        }
+
+
+    }
+    return ans >= n
+}
