bcfed8a7083a8f8825ae7eb85fe0e4cca7ede6c9
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2025-04-10 23:11:31 -0400

    Update 2614_PrimeInDiagonal.c

--- 2614_PrimeInDiagonal.c
+++ 2614_PrimeInDiagonal.c
@@ -1,47 +1,25 @@
-int diagonalPrime(int** nums, int numsSize, int* numsColSize) {
-    int index = 0;
-    int index2 = numsSize - 1;
-    int max = 0;
-    for(int i = 0; i < numsSize; i++){
-        if(nums[i][index] > max){
-            if( nums[i][index] == 2){
-                max = nums[i][index];
-            } else if(nums[i][index] % 2 == 0 || nums[i][index] <= 1){
-                
-            } else {
-                int found = 0;
-                for(int k = 3; k * k <= nums[i][index]; k += 2){
-                    if(nums[i][index] % k == 0){
-                        found = 1;;
-                    }
-                }
-                if(!found){
-                    max = nums[i][index];
-
-                }
-
-            }
+bool prime(int a){
+    if(a==1) return false;
+    int tmp = 2;
+    while(tmp * tmp <= a){
+        if (a % tmp == 0) {
+            return false;
         }
-        if(nums[i][index2] > max){
-            if( nums[i][index2] == 2){
-                max = nums[i][index2];
-            } else if(nums[i][index2] % 2 == 0 || nums[i][index2] <= 1){
-                
-            } else {
-                int found = 0;
-                for(int k = 3; k * k <= nums[i][index2]; k += 2){
-                    if(nums[i][index2] % k == 0){
-                        found = 1;
-                    }
-                }
-                if(!found){
-                    max = nums[i][index2];
-
-                }
-            }
-        }
-        index++;
-        index2--;
+        tmp++;
     }
-    return max;
+    return true;
+}
+
+
+int diagonalPrime(int** nums, int numsSize, int* numsColSize) {
+    int ans = 0;
+    for(int i = 0; i < numsSize; i++){
+        if(prime(nums[i][i])){
+            ans = ans > nums[i][i] ? ans : nums[i][i];
+        }
+        if(prime(nums[i][numsSize-i-1])){
+            ans = ans > nums[i][numsSize-i-1] ? ans : nums[i][numsSize-i-1];
+        }
+    }
+    return ans;
 }
