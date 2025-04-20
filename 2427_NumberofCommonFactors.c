1de5179cb31770907683c2ef4c733651ea81a375
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2025-04-19 23:38:32 -0400

    Create 2427_NumberofCommonFactors.c

---
+++ 2427_NumberofCommonFactors.c
@@ -0,0 +1,9 @@
+int commonFactors(int a, int b) {
+    int ans = 0;
+    for(int i = 1; i <= fmin(a, b); i++){
+        if(a % i == 0 && b % i == 0){
+            ans++;
+        }
+    }
+    return ans;
+}
