ac87c5bb43e113c87548a155fb6c1ccfb8ab70c9
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2025-06-22 20:23:25 -0400

    Update 704_BinarySearch.c

--- 704_BinarySearch.c
+++ 704_BinarySearch.c
@@ -13,3 +13,36 @@ int search(int* nums, int numsSize, int target) {
     }
     return -1;
 }
+
+
+int try(int* nums, int left, int right, int target){
+    if(left > right){
+        return -1;
+    }
+    int mid = left + (right - left) / 2;
+    if(nums[mid] > target){
+        return try(nums, left, mid - 1, target);
+    } else if(nums[mid] < target){
+        return try(nums, mid + 1, right, target);
+    } else {
+        return mid;
+    }
+    return -1;
+
+}
+int search(int* nums, int numsSize, int target) {
+    // int left = 0;
+    // int right = numsSize - 1;
+    // while(left <= right){
+    //     int mid = left + (right - left) / 2;
+    //     if(nums[mid] < target){
+    //         left = mid + 1;
+    //     } else if(nums[mid] > target){
+    //         right = mid - 1;
+    //     } else {
+    //         return mid;
+    //     }
+    // }
+    // return -1;
+    return try(nums, 0, numsSize - 1, target);
+}
