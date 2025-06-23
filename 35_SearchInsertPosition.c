8ff105e598a7953c75c77aabed74fa155c2454e3
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2025-06-22 20:09:48 -0400

    Update 35_SearchInsertPosition.c

--- 35_SearchInsertPosition.c
+++ 35_SearchInsertPosition.c
@@ -15,3 +15,24 @@ int searchInsert(int* nums, int numsSize, int target) {
     }
     return left;
 }
+
+
+int try(int* nums, int left, int right, int target){
+    if(left > right){
+        return left;
+    }
+    int mid = left + (right - left) / 2;
+    if(nums[mid] > target){
+        return try(nums, left, mid - 1, target);
+    } else if(nums[mid] < target){
+        return try(nums, mid + 1, right, target);
+    } else {
+        return mid;
+    }
+    return mid;
+}
+
+int searchInsert(int* nums, int numsSize, int target) {
+
+    return try(nums,0, numsSize - 1, target);
+}
