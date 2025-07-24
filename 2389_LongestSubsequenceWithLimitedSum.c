5f110727e24d8ead9f6bf0af5e8c4e390b165d2d
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2025-07-23 22:29:15 -0400

    Create 2389_LongestSubsequenceWithLimitedSum.c

---
+++ 2389_LongestSubsequenceWithLimitedSum.c
@@ -0,0 +1,34 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+static int cmp(const void *pa, const void *pb) {
+    return *(int *)pa - *(int *)pb;
+}
+
+int check(int *arr, int arrSize, int target) {
+    int low = 1, high = arrSize;
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] > target) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+int* answerQueries(int* nums, int numsSize, int* queries, int queriesSize, int* returnSize) {
+    qsort(nums, numsSize, sizeof(int), cmp);
+    int f[numsSize + 1];
+    f[0] = 0;
+    for (int i = 0; i < numsSize; i++) {
+        f[i + 1] = f[i] + nums[i];
+    }
+    int *answer = (int *)calloc(sizeof(int), queriesSize);
+    for (int i = 0; i < queriesSize; i++) {
+        answer[i] = check(f, numsSize + 1, queries[i]) - 1;
+    }
+    *returnSize = queriesSize;
+    return answer;
+}
