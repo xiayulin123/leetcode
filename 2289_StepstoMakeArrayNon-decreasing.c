9fadda355ac87b2f0ad8563023acde74b09024f0
Yulin Xia <113715923+xiayulin123@users.noreply.github.com>
2025-08-16 21:31:41 -0400

    Update 2289_StepstoMakeArrayNon-decreasing.c

--- 2289_StepstoMakeArrayNon-decreasing.c
+++ 2289_StepstoMakeArrayNon-decreasing.c
@@ -1,35 +1,24 @@
 int totalSteps(int* nums, int numsSize) {
     int ans = 0;
     int* stk = calloc(numsSize, sizeof(int));
-    int top1 = -1;
+    int* cnt = calloc(numsSize, sizeof(int));
     int top = -1;
-    bool finished = false;
-    int n = numsSize;
-    int deleted = 0;
-    while(!finished){
-        finished = true;
-        int x = 0;
-        for(int i = 0; i < n; i++){
-            stk[++top] = nums[i];
-            if(x == 0){
-                if(top >= 1 && nums[i] < stk[top-1]){
-                    x = nums[i];
-                    finished = false;
-                    top--;
-                    deleted++;
-                }
-            } else {
-                if(top >= 1 && nums[i] < x){
-                    x = nums[i];
-                    top--;
-                    deleted++;
-                }
-            }
-
-            
+    for(int i = 0; i < numsSize; i++){
+        int tmp = nums[i];
+        int t = 0;
+        while(top >= 0 && stk[top] <= tmp){
+            if(cnt[top] > t) t = cnt[top];
+            top--;
         }
-        ans++;
-        
+
+        if(top < 0){
+            stk[++top] = tmp;
+            cnt[top] = 0;
+        } else {
+            stk[++top] = tmp;
+            cnt[top] = t + 1;
+            if (cnt[top] > ans) ans = cnt[top];
+        }
     }
-    
+    return ans;
 }
